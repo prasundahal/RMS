b64cbe12339632b9ec3272d9b1990e2fda36b51f
sushant <sdangol1999@gmail.com>
2020-03-06 17:18:44 +0545

    fix

--- routes/web.php
+++ routes/web.php
@@ -28,22 +28,13 @@ Route::resource('iteams', 'IteamController');
 Route::resource('tables', 'TableController');
 
 Route::resource('tableorders', 'TableorderController');
-<<<<<<< HEAD
 //Route::get('tableorders/{id}', 'TableorderController@create')->name('tableorders.create');
 
-<<<<<<< HEAD
-=======
->>>>>>> 972221cf8ecf73cbe4540b6ad90953495cbcd60a
+
 
 Route::group(['middleware' => ['auth']], function() {
     Route::resource('roles','RoleController');
     Route::resource('users','UserController');
 });
 
-<<<<<<< HEAD
-=======
-?>
->>>>>>> 01d604c1903812411534efdd227795d6ba9989c7
-=======
-?>
->>>>>>> 972221cf8ecf73cbe4540b6ad90953495cbcd60a
+
